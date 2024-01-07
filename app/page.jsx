893c01961a1ec4d0385e6d479051aa9f08c7f05f
molhamalrashed <molham.alrd@gmail.com>
2024-01-07 20:58:56 +0100

    second-commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-const page = () => {
+const Home = () => {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover and share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">AI-Powered prompts</span>
+        </h1>
+        <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover cerate and share creative prompts</p>
+        {/* feed */}
+    </section>
   )
 }
 
-export default page
+export default Home
